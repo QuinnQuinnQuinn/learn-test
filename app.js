b4eefb4a9da0cf69240ce5d44e431bca04fd75d5
Kun Cheng <ckw6688@Kuns-iMac.local>
2016-10-15 21:56:31 -0500

    qc-test2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express(); //hello
+var app = express(); //hello world!!
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
